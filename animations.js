fb0794a668713788380668cddffab7b905941369
Gia Alexis Garado <alexisgarado@gmail.com>
2020-03-04 22:35:22 -0800

    fancier navbar animation

--- animations.js
+++ animations.js
@@ -13,29 +13,58 @@ function fadeThree() {
     action.classList.toggle('fadeIn');
 }
 
-function fadeFour() {
+function fade1() {
     var action = document.getElementsByClassName("load4")[0];
     action.classList.toggle('fadeIn');
+}
+
+function fade2() {
     var action = document.getElementsByClassName("load5")[0];
     action.classList.toggle('fadeIn');
+}
+
+function fade3() {
     var action = document.getElementsByClassName("load6")[0];
     action.classList.toggle('fadeIn');
+}
+
+function fade4() {
     var action = document.getElementsByClassName("load7")[0];
     action.classList.toggle('fadeIn');
+}
+
+function fade5() {
     var action = document.getElementsByClassName("load8")[0];
     action.classList.toggle('fadeIn');
+}
+
+function fade6() {
     var action = document.getElementsByClassName("load9")[0];
     action.classList.toggle('fadeIn');
+}
+
+function fade7() {
     var action = document.getElementsByClassName("load10")[0];
     action.classList.toggle('fadeIn');
+}
+
+function fade8() {
     var action = document.getElementsByClassName("load11")[0];
     action.classList.toggle('fadeIn');
 }
+
 
 // display particles as background
 window.onload = function() {
     setTimeout(fadeOne, 500);
     setTimeout(fadeTwo, 560);
     setTimeout(fadeThree, 640);
-    setTimeout(fadeFour, 1000);
+    setTimeout(fade1, 1050);
+    setTimeout(fade2, 1100);
+    setTimeout(fade3, 1150);
+    setTimeout(fade4, 1200);
+    setTimeout(fade5, 1250);
+    setTimeout(fade6, 1300);
+    setTimeout(fade7, 1350);
+    setTimeout(fade8, 1400);
 };
